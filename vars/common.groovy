196ac5f3ab36006fb525aa7712eb2d7865c93ab6
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-03-07 12:15:03 +0530

    updating ./roboshop-jenkins-shared-libraries

--- vars/common.groovy
+++ vars/common.groovy
@@ -3,3 +3,11 @@ def codeQuality() {
         echo 'code quality'
     }
 }
+
+def styleChecks() {
+    if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*" ) {
+        stage ('Style Checks') {
+            echo 'Style Checks'
+        }
+    }
+}
