2a4e79c4f21f26780da28ecb83ce901cbfecc3bd
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-03-08 11:26:18 +0530

    updating ./roboshop-jenkins-shared-libraries

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,9 +1,7 @@
 def call() {
     node {
 
-        if (! env.TAG_NAME) {
-            env.TAG_NAME = ""
-        }
+
         common.codeQuality()
         common.codeChecks()
         common.artifacts()
