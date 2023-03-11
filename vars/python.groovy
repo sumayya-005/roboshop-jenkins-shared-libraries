c38567973e3f6db56c81927ac69784adfd813861
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-03-11 23:56:10 +0530

    updating ./roboshop-jenkins-shared-libraries

--- vars/python.groovy
+++ vars/python.groovy
@@ -3,7 +3,7 @@ def call() {
             try {
                 common.codeCheckout()
                 common.codeQuality()
-                common.codeChecks()
+                common.codeChecks()    
                 common.artifacts()
             } catch (Exception e) {
                 mail bcc: '', body: "Build Failed ${RUN_DISPLAY_URL}", cc: '', from: 'sumayyashaik1005@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'sumayyashaik1005@gmail.com'
