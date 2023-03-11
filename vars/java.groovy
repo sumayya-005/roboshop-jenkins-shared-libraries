d4de1165ff07b1335019ed9ffdc0b153e2d70cc5
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-03-11 23:36:40 +0530

    updating ./roboshop-jenkins-shared-libraries

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,12 +1,19 @@
 def call() {
+    env.SONAR_EXTRA_OPTS = "-Dsonar.java.binaries=./target"
+    env.APPTYPE = "java"
     node {
         try {
             common.codeCheckout()
+            stage('Compile Code') {
+                sh 'mvn compile'
+            }
             common.codeQuality()
             common.codeChecks()
             common.artifacts()
         } catch (Exception e) {
-            mail bcc: '', body: "Build Failed ${RUN_DISPLAY_URL}", cc: '', from: 'sumayyashaik1005@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'sumayyashaik1005@gmail.com'
+            mail bcc: '', body: "Build Failed ${RUN_DISPLAY_URL}", cc: '', from: 'raghuk.vit@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'raghuk.vit@gmail.com'
         }
+
     }
+
 }
