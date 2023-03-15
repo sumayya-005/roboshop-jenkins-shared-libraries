c271888dc3bc559327e34977a94c047185124e12
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-03-15 12:05:57 +0530

    updating ./roboshop-jenkins-shared-libraries

--- vars/common.groovy
+++ vars/common.groovy
@@ -35,9 +35,31 @@ def codeChecks() {
 
 def artifacts() {
     if ( env.TAG_NAME ==~ ".*" ) {
-        stage(' Download Dependencies') {
-            echo 'Download Dependencies'
+
+        stage()
+        if (env.APPTYPE == "nodejs") {
+            sh '''
+          npm install 
+          #zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js 
+        '''
         }
+        if (env.APPTYPE == "java") {
+            sh '''
+          mvn clean package 
+          mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar 
+          #zip -r ${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}.jar
+        '''
+        }
+        if (env.APPTYPE == "python") {
+            sh '''
+          #zip -r ${COMPONENT}-${TAG_NAME}.zip *.py ${COMPONENT}.ini requirements.txt
+        '''
+        }
+        if (env.APPTYPE == "nginx") {
+            sh '''
+          #cd static
+          #zip -r ../${COMPONENT}-${TAG_NAME}.zip *
+        '''
 
         stage('Prepare Artifacts') {
             echo 'Prepare Artifacts'
