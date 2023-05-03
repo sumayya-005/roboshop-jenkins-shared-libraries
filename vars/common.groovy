59b1962de30c4be176edc4d5c36c03b53f350efc
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-03 22:58:56 +0530

    updating ./roboshop-jenkins-shared-libraries

--- vars/common.groovy
+++ vars/common.groovy
@@ -64,5 +64,35 @@ def artifacts() {
             }
         }
 
+        stage('Publish Artifacts') {
+            withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'nexusPass', usernameVariable: 'nexusUser')]) {
+                sh '''
+          curl -v -u ${nexusUser}:${nexusPass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+        '''
+            }
+        }
+
     }
 }
+
+
+def artifacts() {
+    if (env.TAG_NAME ==~ ".*") {
+        stage('Build Docker Image') {
+            sh '''
+               docker build .
+            '''
+        }
+
+        stage('Publish Artifacts') {
+            withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'nexusPass', usernameVariable: 'nexusUser')]) {
+                sh '''
+          curl -v -u ${nexusUser}:${nexusPass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+        '''
+            }
+        }
+
+
+    }
+}
+
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -5,7 +5,8 @@ def call() {
             common.codeCheckout()
             common.codeQuality()
             common.codeChecks()
-            common.artifacts()
+//            common.artifacts()
+            common.docker()
         } catch (Exception e) {
             mail bcc: '', body: "Build Failed ${RUN_DISPLAY_URL}", cc: '', from: 'sumayyashaik1005@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'sumayyashaik1005@gmail.com'
         }
