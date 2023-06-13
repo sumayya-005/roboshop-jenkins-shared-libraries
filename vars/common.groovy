4eec841ab152870bdd6e3c1254702a083b7775b5
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-06-13 17:15:10 +0530

    updating ./roboshop-jenkins-shared-libraries

--- vars/common.groovy
+++ vars/common.groovy
@@ -33,53 +33,53 @@ def codeChecks() {
 }
 
 
-def artifacts() {
-    if (env.TAG_NAME ==~ ".*") {
-
-        stage()
-        stage('Prepare Artifacts') {
-            if (env.APPTYPE == "nodejs") {
-                sh '''
-          npm install 
-          #zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js 
-        '''
-            }
-            if (env.APPTYPE == "java") {
-                sh '''
-          mvn clean package 
-          mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar 
-          #zip -r ${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}.jar
-        '''
-            }
-            if (env.APPTYPE == "python") {
-                sh '''
-          #zip -r ${COMPONENT}-${TAG_NAME}.zip *.py ${COMPONENT}.ini requirements.txt
-        '''
-            }
-            if (env.APPTYPE == "nginx") {
-                sh '''
-          #cd static
-          #zip -r ../${COMPONENT}-${TAG_NAME}.zip *
-        '''
-            }
-        }
-
-        stage('Build Docker Image') {
-            sh '''
-               docker build .
-            '''
-        }
-
-        stage('Publish Artifacts') {
-            withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'nexusPass', usernameVariable: 'nexusUser')]) {
-                sh '''
-          curl -v -u ${nexusUser}:${nexusPass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
-        '''
-            }
-        }
-
-    }
-}
+//def artifacts() {
+//    if (env.TAG_NAME ==~ ".*") {
+//
+//        stage()
+//        stage('Prepare Artifacts') {
+//            if (env.APPTYPE == "nodejs") {
+//                sh '''
+//          npm install
+//          #zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js
+//        '''
+//            }
+//            if (env.APPTYPE == "java") {
+//                sh '''
+//          mvn clean package
+//          mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar
+//          #zip -r ${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}.jar
+//        '''
+//            }
+//            if (env.APPTYPE == "python") {
+//                sh '''
+//          #zip -r ${COMPONENT}-${TAG_NAME}.zip *.py ${COMPONENT}.ini requirements.txt
+//        '''
+//            }
+//            if (env.APPTYPE == "nginx") {
+//                sh '''
+//          #cd static
+//          #zip -r ../${COMPONENT}-${TAG_NAME}.zip *
+//        '''
+//            }
+//        }
+//
+//        stage('Build Docker Image') {
+//            sh '''
+//               docker build .
+//            '''
+//        }
+//
+//        stage('Publish Artifacts') {
+//            withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'nexusPass', usernameVariable: 'nexusUser')]) {
+//                sh '''
+//          curl -v -u ${nexusUser}:${nexusPass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+//        '''
+//            }
+//        }
+//
+//    }
+//}
 
 
 
